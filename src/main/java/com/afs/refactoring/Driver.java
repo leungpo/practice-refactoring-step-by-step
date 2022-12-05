c375c7013bb44a4caffab56e55d83aadefafe4b2
leungpo <polly.leung@oocl.com>
2022-12-05 16:18:27 +0800

    refactor: put isAdult into Driver to eliminate feature envy

--- src/main/java/com/afs/refactoring/Driver.java
+++ src/main/java/com/afs/refactoring/Driver.java
@@ -2,6 +2,7 @@ package com.afs.refactoring;
 
 public class Driver {
     private int age;
+    private static final int MIN_ADULT_AGE = 18;
 
     public Driver(int age) {
         this.age = age;
@@ -9,5 +10,9 @@ public class Driver {
 
     public int getAge() {
         return age;
+    }
+
+    public boolean isAdult(){
+        return getAge() >= MIN_ADULT_AGE;
     }
 }
