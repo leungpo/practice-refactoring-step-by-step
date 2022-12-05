86d8dbaab0026f7310e5b2c6b86fc72f3a86f8a4
leungpo <polly.leung@oocl.com>
2022-12-05 16:21:40 +0800

    refactor: remove unuse name

--- src/main/java/com/afs/refactoring/Police.java
+++ src/main/java/com/afs/refactoring/Police.java
@@ -2,7 +2,6 @@ package com.afs.refactoring;
 
 public class Police {
 
-    public static final int MIN_ADULT_AGE = 18;
 
     public boolean checkDriver(Driver driver) {
         return driver.isAdult();
